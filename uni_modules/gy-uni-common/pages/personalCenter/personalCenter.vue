920b1856d0cf0bb88e0accda109e552f0b227947
guoyao <guoyao132@163.com>
2024-03-29 18:01:29 +0800

    增加版本0.0.7

---
+++ uni_modules/gy-uni-common/pages/personalCenter/personalCenter.vue
@@ -0,0 +1,28 @@
+<template>
+  <view>
+    <uni-list :border="true">
+      <uni-list-item title="版本更新" clickable @click="updateVersion"></uni-list-item>
+    </uni-list>
+  </view>
+</template>
+
+<script>
+import {checkUpdate} from "../../js/common";
+
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    updateVersion() {
+      this.$gyCommon.checkUpdate();
+    },
+  },
+  mounted() {
+  },
+}
+</script>
+
+<style>
+
+</style>
